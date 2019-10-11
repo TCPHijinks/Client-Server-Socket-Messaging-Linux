ebcd833bbfc6634606d447d59fe95e570529826b
Buldron <36693901+Buldron@users.noreply.github.com>
2019-10-11 11:53:18 +1000

    Improved ID reading.
    Improved ID reading so commands such as "SUB 1" & SUB "23" work without leading-zeros.

--- server.c
+++ server.c
@@ -21,6 +21,7 @@
 
 */
 
+
 #define BUFFER_SIZE 2040
 const int MAX_CLIENTS = 1;
 const int MAX_ID_VAL = 255;
@@ -42,10 +43,66 @@ void error(const char *msg)
     exit(1);
 }
 
+static volatile sig_atomic_t servrun = 1;
+static void sig_handler(int _)
+{
+    (void)_;
+    servrun = 0;
+}
+
+char * str_append(char* str1, char* str2) // Append strings and return result.
+{
+    int n = strlen(str1) + strlen(str2);
+    char* t_msg = calloc(n, sizeof(char));
+    strcat(t_msg , str1);
+    strcat(t_msg, str2);
+    
+    return t_msg;
+}
+
+char * replace_str(char *str, char *old, char *new)
+{
+  static char buff[4096];
+  char *p;
+
+  if(!(p = strstr(str, old)))  // Is 'orig' even in 'str'?
+    return str;
+   
+  strncpy(buff, str, p-str); // Copy characters from 'str' start to 'orig' st$
+  buff[p-str] = '\0';
+  sprintf(buff+(p-str), "%s%s", new, p+strlen(old));
+  return buff;
+}
+
+int NumOfIntegerDigits(int number)
+{
+    if(number > 99) { return 3; }
+    if(number > 9) { return 2; }    
+    return 1;
+}
+
+char * ToCharArray(int number)
+{
+    int n = NumOfIntegerDigits(number);
+    char *cArray = calloc(n, sizeof(char));
+    sprintf(cArray, "%d" , number);
+    return cArray;
+}
+
+
+
+
+void WriteClient(int newsockfd, char* buffer) // Write to client.
+{    
+    int n = write(newsockfd , buffer, strlen(buffer));
+    if(n < 0)
+        error("Error on write."); 
+}
+
 struct Message
 {
-    char* chnid;
-    struct Message* next;
+    char* id;
+    char msg[1024];
 };
 
 
@@ -73,73 +130,50 @@ struct Client
 
 
 
-char * toCharArray(int number)
-{
-    int n = 1; // Num of digits in integer.
-    if(number > 9) { n = 2; }
-    if(number > 99) { n = 3; }
-    
-    char *cArray = calloc(n, sizeof(char));
-    sprintf(cArray, "%d" , number);
-    return cArray;
-}
 
 
 
 
 
-char *replace_str(char *str, char *old, char *new)
-{
-  static char buff[4096];
-  char *p;
-
-  if(!(p = strstr(str, old)))  // Is 'orig' even in 'str'?
-    return str;
-   
-  strncpy(buff, str, p-str); // Copy characters from 'str' start to 'orig' st$
-  buff[p-str] = '\0';
-  sprintf(buff+(p-str), "%s%s", new, p+strlen(old));
-  return buff;
-}
-
-
-
-int ValidID(char* id) 
-{     
-    char *t_id = id;
-    if(strlen(t_id) < 3)
-        return -1;  
-    while(*t_id != '\0')
-    {        
-        if(*t_id < '0' || *t_id> '9')
-            return -1;
-        t_id++;    
-    }
-
-    // Convert id to integer.
-    int i_id = 0; 
+// TO DO: WORK WITH LEADING ZEROS (e.g. 004 or 098) && FIX single ASCII defaulting to 0 (e.g. SUB G == SUB 0).
+int ValidID(char* id)  // Verifies that id is completely decimal and inside allowed value range.
+{      
+    int i_id = 0; // ID as converted integer.
     sscanf(id, "%d", &i_id); 
+    
+    if((strlen(id) - 1) > NumOfIntegerDigits(i_id)) // Integer shorter than string if have invalid char and convert. 
+        return -1;
 
     printf("ID VAL is:%d",i_id);
     if(i_id > MAX_ID_VAL || i_id < MIN_ID_VAL) // Fail if id outside allowed range.
         return -1;   
-    
+
     return 0;
 }
 
 
 
-int cli_alreadySubbed(char* chanId) // TO DO: Optimize algorithm later.
+
+char* GetChanIdFromBuffer(char buffer[BUFFER_SIZE], int commandLen) // Gets ID from stdin buffer.
 {    
-    for(int i = 0; i < client.curChanPos; i++)
-    {        
-        if(strcmp(chanId, client.channels[i].id) == 0) 
-        {    
-            return 0; // Already subbed to.
-        }             
-    }  
-    return -1; // No match.
+    int cmdLen = commandLen + 1; // Command + space.
+    
+    char* id = calloc(4, sizeof(char));
+    memcpy(id , &buffer[cmdLen] , 4); // Get id from terminal entry.
+
+    if(ValidID(id) == -1)
+        return id;
+    
+
+    int n = 0;
+    n = strlen(id) - 1;
+
+    int i_id = 0; // ID as converted integer.
+    sscanf(id, "%d", &i_id); 
+    free(id);
+    return ToCharArray(i_id);
 }
+
 
 
 
@@ -153,7 +187,7 @@ void AddNewChan(char* idchan)
 }
 
 
-int GetChanIndex(char* id)
+int GetChanIndex(char* id) // Return index of given channel.
 {
     int n = client.curChanPos;
     for(int i = 0; i < n; i++)
@@ -165,18 +199,20 @@ int GetChanIndex(char* id)
 }
 
 
+
+
 char* Unsub(char buffer[BUFFER_SIZE])
 {
-    char id[4];
-    memcpy(id , &buffer[7] , 4); // Get 3 char long id inside of <> + /0 end.
-    id[3] = '\0'; 
+    char* id = GetChanIdFromBuffer(buffer, 5);
+   
    
     if(ValidID(id) < 0) // Invalid id.
-        return replace_str("Invalid channel: <xxx>.\n","xxx",id);
+        return replace_str("Invalid channel: <xxx>\n","xxx",id);
     
+
     int i = GetChanIndex(id);
     if(i < 0) // Not subscribed to this chan id.
-        return replace_str("Not subscribed to channel: <xxx>.\n","xxx",id);
+        return replace_str("Not subscribed to channel: xxx.\n","xxx",id);
     
     
    
@@ -185,8 +221,7 @@ char* Unsub(char buffer[BUFFER_SIZE])
         strcpy(client.channels[i].id , client.channels[i+1].id);
         client.channels[i].curMsgReadPos = client.channels[i+1].curMsgReadPos;
         client.channels[i].totalMsgs = client.channels[i+1].totalMsgs;         
-        memcpy(client.channels[i].msgs, client.channels[i+1].msgs, sizeof(client.channels[i].msgs));   
-        printf("\nwork\n");     
+        memcpy(client.channels[i].msgs, client.channels[i+1].msgs, sizeof(client.channels[i].msgs));  
     }
     if(client.curChanPos > 0)
         client.curChanPos -= 1;
@@ -196,54 +231,28 @@ char* Unsub(char buffer[BUFFER_SIZE])
 
 
 
+
 char* Sub(char buffer[BUFFER_SIZE])
 {       
-    char id[4];
-    memcpy(id , &buffer[5] , 4); // Get 3 char long id inside of <>.
-    id[3] = '\0'; // Set string end.
+    char* id = GetChanIdFromBuffer(buffer, 3);
 
     if(ValidID(id) < 0) // Return if invalid id.
-        return replace_str("Invalid channel: <xxx>.\n","xxx",id);
+        return replace_str("Invalid -channel: xxx\n","xxx",id);
 
-    if(cli_alreadySubbed(id) == 0)
-        return replace_str("Already subscribed to channel <xxx>.\n","xxx",id);
+    if(GetChanIndex(id) >= 0)
+        return replace_str("Already subscribed to channel xxx.\n","xxx",id);
     
     AddNewChan(id);
-    return replace_str("Subscribed to channel <xxx>.\n","xxx",id);
+    return replace_str("Subscribed to channel xxx.\n","xxx",id);
 }
 
 
 
-// Handler for program exit via ctrl+c in terminal.
-static volatile sig_atomic_t servrun = 1;
-static void sig_handler(int _)
-{
-    (void)_;
-    servrun = 0;
-}
 
 
 
-// Append strings and return result.
-char* str_append(char* str1, char* str2)
-{
-    int n = strlen(str1) + strlen(str2);
-    char* t_msg = calloc(n, sizeof(char));
-    strcat(t_msg , str1);
-    strcat(t_msg, str2);
-    
-    return t_msg;
-}
 
 
-
-// Write to client.
-void writecli(int newsockfd, char* buffer)
-{    
-    int n = write(newsockfd , buffer, strlen(buffer));
-    if(n < 0)
-        error("Error on write."); 
-}
 
 
 
@@ -262,11 +271,11 @@ char* get_cli_channels_msg()
     strcpy(msg, "Channel Subscriptions\n"); 
     for(int i = 0; i < client.curChanPos; i++)
     {
-        char* t_msg = "Channel: <111>\tTotal Messages: 222\tRead: 333\tUnread 444\n";        
+        char* t_msg = "Channel: 111\tTotal Messages: 222\tRead: 333\tUnread 444\n";        
         t_msg = replace_str(t_msg, "111", client.channels[i].id);
-        t_msg = replace_str(t_msg, "222", toCharArray(client.channels[i].totalMsgs));
-        t_msg = replace_str(t_msg, "333", toCharArray(client.channels[i].curMsgReadPos));
-        t_msg = replace_str(t_msg, "444", toCharArray(client.channels[i].totalMsgs - client.channels[i].curMsgReadPos));
+        t_msg = replace_str(t_msg, "222", ToCharArray(client.channels[i].totalMsgs));
+        t_msg = replace_str(t_msg, "333", ToCharArray(client.channels[i].curMsgReadPos));
+        t_msg = replace_str(t_msg, "444", ToCharArray(client.channels[i].totalMsgs - client.channels[i].curMsgReadPos));
         strcpy(msg, str_append(msg, t_msg));
      
     }
@@ -322,7 +331,7 @@ int main(int argc, char * argv[])
     strcpy(client.id, "001") ; //////
     
     // Starting welcome message.
-    writecli(newsockfd, replace_str("Welcome! Your client ID is <xxx>.\n","xxx",client.id));
+    WriteClient(newsockfd, replace_str("Welcome! Your client ID is xxx.\n","xxx",client.id));
 
 
     while(servrun)
@@ -347,7 +356,7 @@ int main(int argc, char * argv[])
         }
         
         // Write to client.
-        writecli(newsockfd, msg);   
+        WriteClient(newsockfd, msg);   
     }
         
     close(newsockfd);
