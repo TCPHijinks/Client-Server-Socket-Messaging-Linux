b3db3dcf0fbfcd88a3b03998d49218139e43e9bf
Buldron <36693901+Buldron@users.noreply.github.com>
2019-10-11 13:30:09 +1000

    Limited Send Functionality.
    Works for following format "SEND XXX MESSAGE". Need to fix how ID is validated so it works for "SEND X MESSAGE".

--- server.c
+++ server.c
@@ -101,7 +101,6 @@ void WriteClient(int newsockfd, char* buffer) // Write to client.
 
 struct Message
 {
-    char* id;
     char msg[1024];
 };
 
@@ -109,17 +108,13 @@ struct Message
 struct Channel
 {
     char id[3];
+    int curSubPos; // From where you can read future messages, set on sub.
     int curMsgReadPos;
     int totalMsgs;
-    struct Message msgs[100];
+    struct Message msgs[255];
 };
 
-void chan_addMsg(struct Message msg)
-{
-    // Have buffer overflow in case of too many messages.
-}
-
-
+struct Channel* allChans;
 
 struct Client
 {
@@ -154,12 +149,12 @@ int ValidID(char* id)  // Verifies that id is completely decimal and inside allo
 
 
 
-char* GetChanIdFromBuffer(char buffer[BUFFER_SIZE], int commandLen) // Gets ID from stdin buffer.
+char* GetBufferString(char buffer[BUFFER_SIZE], int cmdLen, int targetLen) // Gets ID from stdin buffer.
 {    
-    int cmdLen = commandLen + 1; // Command + space.
+    int len = cmdLen + 1; // Command + space.
     
     char* id = calloc(4, sizeof(char));
-    memcpy(id , &buffer[cmdLen] , 4); // Get id from terminal entry.
+    memcpy(id , &buffer[len] , targetLen); // Get id from terminal entry.
 
     if(ValidID(id) == -1)
         return id;
@@ -177,14 +172,9 @@ char* GetChanIdFromBuffer(char buffer[BUFFER_SIZE], int commandLen) // Gets ID f
 
 
 
-void AddNewChan(char* idchan)
-{
-    strcpy(client.channels[client.curChanPos].id, idchan);
-    client.channels[client.curChanPos].curMsgReadPos = 0;  
-    client.channels[client.curChanPos].totalMsgs = 0;
-    client.curChanPos++;    
-    return;
-}
+
+
+
 
 
 int GetChanIndex(char* id) // Return index of given channel.
@@ -203,7 +193,7 @@ int GetChanIndex(char* id) // Return index of given channel.
 
 char* Unsub(char buffer[BUFFER_SIZE])
 {
-    char* id = GetChanIdFromBuffer(buffer, 5);
+    char* id = GetBufferString(buffer, 5, 4);
    
    
     if(ValidID(id) < 0) // Invalid id.
@@ -231,19 +221,58 @@ char* Unsub(char buffer[BUFFER_SIZE])
 
 
 
-
+// TO DO: Change Client Channels * to a "char* chans[3]" for efficency, also set it so starting read pos after last sent msg when sub.
 char* Sub(char buffer[BUFFER_SIZE])
 {       
-    char* id = GetChanIdFromBuffer(buffer, 3);
+    char* id = GetBufferString(buffer, 3, 4);
 
     if(ValidID(id) < 0) // Return if invalid id.
-        return replace_str("Invalid -channel: xxx\n","xxx",id);
+        return replace_str("Invalid channel: xxx\n","xxx",id);
 
     if(GetChanIndex(id) >= 0)
         return replace_str("Already subscribed to channel xxx.\n","xxx",id);
     
-    AddNewChan(id);
+    
+    int i_id = atoi(id);
+
+    // Set so when sub, start reading after latest msg.
+    allChans[i_id].curSubPos = allChans[i_id].totalMsgs;
+    allChans[i_id].curMsgReadPos = allChans[i_id].totalMsgs;
+
+    // Add server info to client sub list (replace with char* arr[3] eventually).
+    strcpy(client.channels[client.curChanPos].id, allChans[i_id].id);
+    client.channels[client.curChanPos].curSubPos = allChans[i_id].totalMsgs;
+    client.channels[client.curChanPos].curMsgReadPos = allChans[i_id].totalMsgs; 
+    client.channels[client.curChanPos].totalMsgs = allChans[i_id].totalMsgs;
+    client.curChanPos++;    
+
     return replace_str("Subscribed to channel xxx.\n","xxx",id);
+}
+
+
+
+
+
+char* Send(char buffer[BUFFER_SIZE])
+{
+    char* id = GetBufferString(buffer, 4, 4);
+    int len = strlen(buffer) - 9;
+    if(len > 1024)
+        len = 1024;
+    if (len < 0)
+        len = 0;
+    
+    char* msg = GetBufferString(buffer, 8, len);// Get 1024 chars after "SEND XXX " cmd as a msg.
+
+    if(ValidID(id) < 0) // Return if invalid id.
+        return replace_str("Invalid channel: xxx\n","xxx",id);
+
+    int i_id = atoi(id); // ID as integer.
+    struct Message m; // Message to send.
+
+    strcpy(m.msg, msg);
+    strcpy(allChans[i_id].msgs[allChans[i_id].totalMsgs].msg, m.msg);
+    allChans[i_id].totalMsgs++;
 }
 
 
@@ -256,12 +285,8 @@ char* Sub(char buffer[BUFFER_SIZE])
 
 
 
-
-
-
-
 // Return list of subscribed channels.
-char* get_cli_channels_msg()
+char* Channels()
 {
     printf("CUR POS is %d",client.curChanPos);
     if(client.curChanPos <= 0)
@@ -271,11 +296,13 @@ char* get_cli_channels_msg()
     strcpy(msg, "Channel Subscriptions\n"); 
     for(int i = 0; i < client.curChanPos; i++)
     {
+        int i_id = atoi(client.channels[i].id); // Convert id of sub channels to int.
+
         char* t_msg = "Channel: 111\tTotal Messages: 222\tRead: 333\tUnread 444\n";        
-        t_msg = replace_str(t_msg, "111", client.channels[i].id);
-        t_msg = replace_str(t_msg, "222", ToCharArray(client.channels[i].totalMsgs));
-        t_msg = replace_str(t_msg, "333", ToCharArray(client.channels[i].curMsgReadPos));
-        t_msg = replace_str(t_msg, "444", ToCharArray(client.channels[i].totalMsgs - client.channels[i].curMsgReadPos));
+        t_msg = replace_str(t_msg, "111", allChans[i_id].id); // Use converted id to get live channel info (e.g. id of 5 is used as index 5 to get info)
+        t_msg = replace_str(t_msg, "222", ToCharArray(allChans[i_id].totalMsgs));
+        t_msg = replace_str(t_msg, "333", ToCharArray(allChans[i_id].curSubPos - allChans[i_id].curMsgReadPos));
+        t_msg = replace_str(t_msg, "444", ToCharArray(allChans[i_id].curSubPos - allChans[i_id].curMsgReadPos));
         strcpy(msg, str_append(msg, t_msg));
      
     }
@@ -324,11 +351,20 @@ int main(int argc, char * argv[])
     if(newsockfd < 0)
         error("Error on Accept,\n");
   
+     // Channel server setup.
+    allChans = calloc(255, sizeof(struct Channel));    
+    for(int i = 0; i < 255; i++)
+    {
+        strcpy(allChans[i].id, ToCharArray(i));
+        allChans[i].curMsgReadPos = 0;
+        allChans[i].totalMsgs = 0;
+        allChans[i].curSubPos = 0;
+    }
+   
 
-    // Client setup.
-    //client.id = calloc(3, sizeof(char));
+    // Client setup.    
     client.curChanPos = 0;
-    strcpy(client.id, "001") ; //////
+    strcpy(client.id, "1") ; //////
     
     // Starting welcome message.
     WriteClient(newsockfd, replace_str("Welcome! Your client ID is xxx.\n","xxx",client.id));
@@ -349,7 +385,9 @@ int main(int argc, char * argv[])
         else if(strstr(buffer , "SUB") != NULL)                
             strcpy(msg , Sub(buffer)); 
         else if(strstr(buffer , "CHANNELS") != NULL)
-            strcpy(msg, get_cli_channels_msg());
+            strcpy(msg, Channels());
+        else if(strstr(buffer , "SEND") != NULL)
+            strcpy(msg, Send(buffer));
         else
         {
             strcpy(msg , "INVALID INPUT.\n");
